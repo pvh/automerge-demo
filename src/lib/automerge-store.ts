a6e45fcadc8c2b5473e4e9389f13e2eb78f6e058
Speros Kokenes <speroskokenes@gmail.com>
2021-04-01 15:28:19 -0500

    persistence WIP

--- src/lib/automerge-store.ts
+++ src/lib/automerge-store.ts
@@ -1,47 +1,59 @@
-import { writable } from 'svelte/store'
-import { Frontend, ChangeFn } from 'automerge'
+import { writable } from "svelte/store";
+import { Frontend, ChangeFn } from "automerge";
 
-import AutomergeWorker from './worker.ts?worker'
+import AutomergeWorker from "./worker.ts?worker";
+import PersistenceWorker from "./shared-worker.ts?worker";
 
-  var myWorker = new SharedWorker("worker.js");
-import type { FrontendToBackendMessage, BackendToFrontendMessage } from './types'
+import type {
+  FrontendToBackendMessage,
+  BackendToFrontendMessage,
+} from "./types";
 
-const worker = new AutomergeWorker()
+const worker = new AutomergeWorker();
 
-export function openDoc(docId: string) { 
-  const { subscribe, update } = writable(Frontend.init())
+// const urlParams = new URLSearchParams(window.location.search);
+// const shouldStartPersistenceWorker = urlParams.get("persistence") === "true";
+// if (shouldStartPersistenceWorker) {
+//   const persistenceWorker = new PersistenceWorker();
+// }
 
-  function sendWorkerMessage(worker: Worker, message: FrontendToBackendMessage) {
-    worker.postMessage(message)
+export function openDoc(docId: string) {
+  const { subscribe, update } = writable(Frontend.init());
+
+  function sendWorkerMessage(
+    worker: Worker,
+    message: FrontendToBackendMessage
+  ) {
+    worker.postMessage(message);
   }
-  
+
   worker.onmessage = (event: MessageEvent) => {
-    const message: BackendToFrontendMessage = event.data
+    const message: BackendToFrontendMessage = event.data;
     // this is wrong -- we should dispatch more deliberately
     if (message.docId === docId) {
-      update(doc => Frontend.applyPatch(doc, message.patch))
+      update((doc) => Frontend.applyPatch(doc, message.patch));
     }
-  }
-  
+  };
+
   sendWorkerMessage(worker, {
     type: "OPEN",
-    docId
+    docId,
   });
 
   function change(changeFn: ChangeFn<unknown>) {
-    update(doc => {
+    update((doc) => {
       const [newDoc, changeData] = Frontend.change(doc, changeFn);
       sendWorkerMessage(worker, {
         type: "LOCAL_CHANGE",
         docId,
         payload: changeData,
       });
-      return newDoc
-    })
+      return newDoc;
+    });
   }
 
-	return {
-		subscribe,
-		change
-	};
+  return {
+    subscribe,
+    change,
+  };
 }
--- src/lib/worker.ts
+++ src/lib/worker.ts
@@ -1,56 +1,93 @@
-import { Backend } from "automerge"
-import type { BackendState } from "automerge"
-import type { FrontendToBackendMessage, BackendToFrontendMessage, SyncState, GrossEventDataProtocol } from "./types"
-import { encodeMessage, decodeMessage, receiveMessage } from "./protocol"
+import { Backend, decodeChange } from "automerge";
+import type { BackendState } from "automerge";
+import type {
+  FrontendToBackendMessage,
+  BackendToFrontendMessage,
+  SyncState,
+  GrossEventDataProtocol,
+} from "./types";
+import { encodeMessage, decodeMessage, receiveMessage } from "./protocol";
+import { DB } from "./db";
 
 // ERRRRR
-const workerId = Math.round(Math.random() * 1000)
+const workerId = Math.round(Math.random() * 1000);
 
-const backends: { [docId: string]: BackendState } = {}
-const syncStates: { [peerId: string]: SyncState } = {} 
-// must we store these on disk? 
+const db = new DB();
+
+const backends: { [docId: string]: BackendState } = {};
+const syncStates: { [peerId: string]: SyncState } = {};
+// must we store these on disk?
 // how are they corrected aside if they go funky aside from somehow successfully syncing the whole repo?
 
 // This function is mostly here to give me type checking on the communication.
 const sendMessageToRenderer = (message: BackendToFrontendMessage) => {
-  postMessage(message)
-}
+  postMessage(message);
+};
 
 // Respond to messages from the frontend document
 addEventListener("message", (evt: any) => {
-  const data: FrontendToBackendMessage = evt.data
-  const { docId } = data
+  const data: FrontendToBackendMessage = evt.data;
+  const { docId } = data;
 
   if (data.type === "OPEN") {
     backends[docId] = Backend.init();
+    db.getChanges(docId).then((encodedChanges: Uint8Array[]) => {
+      const [newBackend, patch] = Backend.applyChanges(
+        backends[docId],
+        encodedChanges
+      );
+      backends[docId] = newBackend;
+      sendMessageToRenderer({ docId, patch });
+    });
 
     // broadcast a request for it
-    const syncMessage = Backend.syncStart(backends[docId])
-    const encoded = Backend.encodeSyncMessage(syncMessage)
-    sendMessage({docId, source: workerId, encoded: { type: "sync", payload: encoded }})
-  }
-  
-  else if (data.type === "LOCAL_CHANGE") {
+    const syncMessage = Backend.syncStart(backends[docId]);
+    const encoded = Backend.encodeSyncMessage(syncMessage);
+    sendMessage({
+      docId,
+      source: workerId,
+      encoded: { type: "sync", payload: encoded },
+    });
+  } else if (data.type === "LOCAL_CHANGE") {
+    console.log("LOCAL_CHANGE", data.payload);
     const [newBackend, patch, change] = Backend.applyLocalChange(
       backends[docId],
       data.payload
     );
-    backends[docId] = newBackend
-    sendMessageToRenderer({ docId, patch })
+    backends[docId] = newBackend;
+    sendMessageToRenderer({ docId, patch });
 
     // Because we only apply messages when we get a sync, we have to send a sync to everyone
     // and because sync is per-peer, we have to calculate a separate sync-state for everyone (or send a full sync msg).
     // But we don't actually need to send a sync message! The other peer is a stranger.
-    sendMessage({docId, source: workerId, encoded: { type: "change", payload: change}})
+    sendMessage({
+      docId,
+      source: workerId,
+      encoded: { type: "change", payload: change },
+    });
 
-    const syncMessage = Backend.encodeSyncMessage(Backend.syncStart(backends[docId]))
-    sendMessage({docId, source: workerId, encoded: { type: "sync", payload: syncMessage}})
+    /*
+      docId,
+      change,
+
+    */
+    const decodedChange = decodeChange(change);
+    console.log("CHANGE", change, decodedChange);
+    db.storeChange(docId, (decodedChange as any).hash, change);
+
+    const syncMessage = Backend.encodeSyncMessage(
+      Backend.syncStart(backends[docId])
+    );
+    sendMessage({
+      docId,
+      source: workerId,
+      encoded: { type: "sync", payload: syncMessage },
+    });
   }
 });
 
 // In real life, you'd open a websocket or a webRTC thing, or ... something.
-export const channel = new BroadcastChannel('automerge-demo-peer-discovery')
-
+export const channel = new BroadcastChannel("automerge-demo-peer-discovery");
 
 // the changes from Backend.syncResponse (et al) could be flavored arrays for type safety
 export function sendMessage(encodedMessage: GrossEventDataProtocol) {
@@ -58,26 +95,41 @@ export function sendMessage(encodedMessage: GrossEventDataProtocol) {
 }
 
 channel.addEventListener("message", (evt: any) => {
-  const { docId, source, target, encoded } = evt.data as GrossEventDataProtocol
-  
+  const { docId, source, target, encoded } = evt.data as GrossEventDataProtocol;
+
   // TODO: uh, don't broadcast stuff that's just for one peer
-  if (target && target != workerId) { /* not for us */ return }
-
-  const decoded = decodeMessage(encoded)
-  const backend = backends[docId]
-  const syncState = syncStates[source] || { lastSync: [], waitingChanges: [] }
-
-  // we aren't tracking this document yet.
-  if (!backend) { return }
-
-  const [newBackend, newSyncState, patch, outboundMessages] = receiveMessage(decoded, backend, syncState)
-
-  if (patch) { 
-    sendMessageToRenderer({docId, patch})
+  if (target && target != workerId) {
+    /* not for us */ return;
   }
 
-  backends[docId] = newBackend
-  syncStates[source] = newSyncState
+  const decoded = decodeMessage(encoded);
+  const backend = backends[docId];
+  const syncState = syncStates[source] || { lastSync: [], waitingChanges: [] };
 
-  outboundMessages.forEach((decoded) => sendMessage({docId, source: workerId, target: source, encoded: encodeMessage(decoded)}))
-})
+  // we aren't tracking this document yet.
+  if (!backend) {
+    return;
+  }
+
+  const [newBackend, newSyncState, patch, outboundMessages] = receiveMessage(
+    decoded,
+    backend,
+    syncState
+  );
+
+  if (patch) {
+    sendMessageToRenderer({ docId, patch });
+  }
+
+  backends[docId] = newBackend;
+  syncStates[source] = newSyncState;
+
+  outboundMessages.forEach((decoded) =>
+    sendMessage({
+      docId,
+      source: workerId,
+      target: source,
+      encoded: encodeMessage(decoded),
+    })
+  );
+});
